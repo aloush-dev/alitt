6f99f13bb891e67a9c4de6e6155b045769099211
alousha <itsaliabdallah@gmail.com>
2022-06-14 14:01:20 +0100

    add votes feature

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,10 +1,8 @@
 import styles from "../styles/articlecard.module.css";
-import { Link, useParams } from "react-router-dom";
-import { getArticleByID } from "../utils/api";
-import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { FaComments } from "react-icons/fa";
 
 export const ArticleCard = ({ article }) => {
-
   return (
     <Link to={`/articles/article/${article.article_id}`}>
       <li className={styles.articlecard}>
@@ -19,7 +17,12 @@ export const ArticleCard = ({ article }) => {
         </div>
 
         <div className={styles.articlevotes}>{article.votes}</div>
-        <div className={styles.articlecomments}>{article.comment_count}</div>
+        <div className={styles.articlecomments}>
+          <div className={styles.commentsicon}>
+            <FaComments />
+          </div>
+          {article.comment_count}
+        </div>
       </li>
     </Link>
   );
