b09fc55b33c6ad683bc04e471110ebb9787f05d6
alousha <itsaliabdallah@gmail.com>
2022-06-17 15:33:12 +0100

    only allows logged in users to upvote articles

--- src/components/ArticleVotesTracker.jsx
+++ src/components/ArticleVotesTracker.jsx
@@ -1,25 +1,33 @@
 import styles from "../styles/articlevotestracker.module.css";
 import { updateVotes } from "../utils/api";
 import { AiFillFire } from "react-icons/ai";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { userContext } from "../contexts/user";
 
 export const ArticleVoteTracker = ({ votes, articleID }) => {
   const [currentVotes, setCurrentVotes] = useState(votes);
 
+  const { user } = useContext(userContext);
+
   return (
     <div className={styles.votebanner}>
-      <button
-        onClick={() => {
-          updateVotes(articleID).then((data) => {
-            if (!data) {
-              alert("Couldn't Update Votes");
-            }
-            setCurrentVotes(data);
-          });
-        }}
-      >
-        <AiFillFire />
-      </button>
+      {user.username ? (
+        <button
+          onClick={() => {
+            updateVotes(articleID).then((data) => {
+              if (!data) {
+                alert("Couldn't Update Votes");
+              }
+              setCurrentVotes(data);
+            });
+          }}
+        >
+          <AiFillFire />
+        </button>
+      ) : (
+        ""
+      )}
+
       <div className={styles.votes}>{currentVotes}</div>
     </div>
   );
